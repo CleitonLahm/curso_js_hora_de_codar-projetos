7d49245659b7f5756142e0c5f463f5f987ac9b20
Cleiton Lahm <cleijllahm@gmail.com>
2023-08-04 11:39:11 -0300

    a

---
+++ fundamentos.js
@@ -0,0 +1,34 @@
+// FUNDAMENTOS
+
+var minhaVariavel = 'ola, amigos';
+console.log(minhaVariavel);
+
+//variaveis e tipos de dados
+
+var meuNumero = 10;
+
+console.log(meuNumero + 10);
+
+console.log(typeof meuNumero);
+
+var meuObjeto = {};
+var meuArray = [];
+var meuNull = null;
+var meuUndefined = undefined;
+
+console.log(typeof meuObjeto);
+console.log(typeof meuArray);
+console.log(typeof meuNull);
+console.log(typeof meuUndefined);
+
+
+// let e const
+
+let x = 5; // nao muda
+const y = 10;
+
+// operadores
+
+
+
+
