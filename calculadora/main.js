5d4dde459cf518a8c9119245457173d560787422
Cleiton Lahm <cleijllahm@gmail.com>
2023-08-09 11:38:12 -0300

    js2

--- calculadora/main.js
+++ calculadora/main.js
@@ -3,7 +3,7 @@ const display = document.querySelector('#displayInput');
 const botaoIgual = document.querySelector('.igual');
 const botaoPonto = document.querySelector('.ponto');
 const botoesNumeros = document.querySelectorAll('.num');
-const botoesOperadores = document.querySelector('.operador');
+const botoesOperadores = document.querySelectorAll('.operador');
 
 
 // variaveis globais
@@ -30,5 +30,58 @@ function insereNumero(evento) {
   atualizaDisplay()
 }
 
+function inserePonto(){
+  if(operacaoAtual.indexOf(".") === -1){
+    operacaoAtual += ".";
+    atualizaDisplay()
+  }
+}
+
+function insereOperador(evento){
+  
+  if(operacaoAtual !== ''){
+    if(!calculando){
+      if(operador !== null){
+        calcula()
+      }
+      else{
+        valorAnterior = operacaoAtual;
+        operacaoAtual = '';
+      }
+    }
+    operador = evento.target.textContent;
+  }
+}
+
+function calcula(){
+  let resultado = null;
+  const operandoAnterior = parseFloat(valorAnterior)
+  const operandoAtual = parseFloat(operacaoAtual)
+
+  switch(operador){
+    case "+":
+      resultado = operandoAnterior + operacaoAtual;
+      break;
+    case "-":
+      resultado = operandoAnterior - operacaoAtual;
+      break;
+    case "*":
+      resultado = operandoAnterior * operacaoAtual;
+      break;
+    case "/":
+      resultado = operandoAnterior / operacaoAtual;
+      break;
+  }
+
+  operacaoAtual = String(resultado);
+  valorAnterior = operacaoAtual;
+  calculando = true;
+  atualizaDisplay()
+}
+
+
 // eventos
+botaoPonto.addEventListener("click", inserePonto)
 botoesNumeros.forEach((botao) => botao.addEventListener('click', insereNumero));
+botoesOperadores.forEach((botao) => botao.addEventListener('click', insereOperador))
+botaoIgual.addEventListener('click', calcula);
